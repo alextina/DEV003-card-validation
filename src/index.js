e30caf4515428b5c2f3e1046da57a5475fc54a94
Alejandra Castillo <castilloavilaa@gmail.com>
2022-12-13 00:18:55 -0500

    Se actulizo el diseño de la página a través de HTML y CSS. Además se limió el código y se quitaron los comentarios.

--- src/index.js
+++ src/index.js
@@ -1,45 +1,21 @@
 import validator from './validator.js';
-// hacer que el botón haga una acción >> alert("Le di clic al botón.")
-// const buttonMessage = document.getElementById("validationButton");
-// buttonMessage.addEventListener("click",(e)=>{
-//     e.preventDefault();
-//     console.log("Le di clic al botón."); //solo funciona si lo pongo arriba
-//     alert("Le di clic al botón.");
-// }); 
 
-//Esto SI funciona pero con el arrow function no funciona lo que puse en input, debo convertirlo en una función.
-//variable que utilizaremos para ejecutar el evento (id del botón)
 const buttonValidation = document.getElementById("validationButton");
-//variablequequeremosqueescuche.addEventListener("evento",acción que escucha);
 buttonValidation.addEventListener("click", (e) => {
-  
-  e.preventDefault(); //cancela el evento y nos permite llamarlo cuando sea necesario
-  //variable que nos permite acceder a la info brindada por el usuario (id del imput)
+  e.preventDefault();
   const inputNumber = document.getElementById("credictCardNumber").value;
-  //variable en la que aplicamos la función isValid (validator.isValid(inputNumber)<< llamamos a la función desde validator.js)
-  //si la variable inputNumber esta vacia, es menor a 13 o no es un npumero, entonces mandar una alerta
-  // return termina la funcion, de lo contrario contínua el flujo
   if (inputNumber.length < 13) {
     alert(`Por favor, ingresa un número de tarjeta.`);
-    return 
-  } 
-  // si la variable no es un numero, entonces terminar la función
+    return
+  }
   const answer = validator.isValid(inputNumber);
-  //crear un condicional para mostrarle una respuesta al usuario
-  if (answer) { // >>> si answer === true entonces
-    //variable que tiene alojado el texto que mostraremos al usuario
+  if (answer) {
     const validCard = (`La tarjeta ${validator.maskify(inputNumber)} es válida.`);
     const answerHtml = document.getElementById("credictCardResult");
     answerHtml.innerHTML = validCard;
-    //alert("Tu tarjeta es válida.")
   } else {
-    //variable que tiene alojado el texto que mostraremos al usuario
     const invalidCard = (`La tarjeta ${validator.maskify(inputNumber)} es inválida.`);
     const answerHtml = document.getElementById("credictCardResult");
     answerHtml.innerHTML = invalidCard;
-    //alert("Tu tarjeta es inválida.")
   }
-
 });
-
-//console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,15 +1,14 @@
 const validator = {
 
   isValid: (credictCardNumber) => {
-    const newCardNumber = credictCardNumber.split("");
-    const reversedCard = newCardNumber.reverse().map(x => parseInt(x));
+    const newCardNumber = credictCardNumber.split("").reverse().map(x => parseInt(x));
     let result = 0;
-    for (let i = 0; i < reversedCard.length; i++) {
-      if (i === 0 || i % 2 === 0) {
-        const noMultiply = (reversedCard[i]);
+    for (let i = 0; i < newCardNumber.length; i++) {
+      if (i % 2 === 0) {
+        const noMultiply = (newCardNumber[i]);
         result += noMultiply;
       } else {
-        const multiply = (reversedCard[i] * 2);
+        const multiply = (newCardNumber[i] * 2);
         if (multiply >= 10) {
           result += multiply - 9;
         } else {
@@ -17,7 +16,7 @@ const validator = {
         }
       }
     }
-    result = result % 10 ? false : true
+    result = result % 10 === 0 ? true : false
     return result;
   },
 
@@ -40,64 +39,3 @@ const validator = {
 
 };
 export default validator;
-//------------------------------------------------------------------------------------------------
-// //crear un prompt variable para la entrada del número de la tarjeta
-// //let credictCardNumber ="4083952015263";
-//let credictCardNumber = (prompt("Ingresar número de tarjeta"));
-
-// //crear función "isValid" son el parámetro "cardNumber"
-// function isValid (credictCardNumber){
-
-//   //convertir el número en array para poder acceder a su posición a través del método "Array.from"
-//let newCardNumber = (Array.from(credictCardNumber));
-// //console.log(newCardNumber)
-
-// //cambiar el orden del arreglo y además parsear el arreglo a través del método "map"
-//let reversedCard = newCardNumber.reverse().map(x=>parseInt(x));
-// //console.log(reversedCard);
-
-// //crear la variable "result" que mostrara la suma de los elementos que cumplan las condiciones del algoritmo de luhn
-// let result = 0;
-// for (let i = 0; i < reversedCard.length; i++) {
-//   if (i === 0 || i % 2 === 0) {
-//     let noMultiply = (reversedCard[i]);
-//     result += noMultiply;
-//   } else {
-//     let multiply = (reversedCard[i] * 2);
-//     if (multiply >= 10) {
-//       result += multiply - 9;
-//     } else {
-//       result += multiply;
-//     }
-//   }
-// }
-// //si no se cumple que el resultado es multiplo de 10 es falso, sino es verdadero
-// result = result % 10 ? false : true
-// return result;
-// }
-// alert(isValid(cardNumber));
-//------------------------------------------------------------------------------------------------
-// let credictCardNumber = (prompt("Ingresar número de tarjeta"));
-
-// function maskify(credictCardNumber) {
-//   let newCardNumber = credictCardNumber.split("");
-//   let size = newCardNumber.length;
-//   let lastPosition = size - 1;
-//   //creando una variable que almacenará los datos
-//   result = "";
-//   //creando un loop para iterar un if else.
-//   for (let i = 0; i <= lastPosition; i++) {
-//     //si (i) es el total de posiciones - 4, ponerle la mascara #
-//     if (i < size - 4) {
-//       let mask = ("#");
-//       //console.log(mask);
-//       result += mask;
-//     } else {
-//       let unMask = (newCardNumber[i]);
-//       //console.log(unMask);
-//       result += unMask;
-//     }
-//   }
-//   return result;
-// }
-// alert(maskify(credictCardNumber));
